a2fb976ff7ae49840b3c90e42b21cb7084fdaf09
gigi.upNorth <leung.gerry@gmail.com>
2022-10-03 23:27:49 -0400

    [#3] Conditional Rendering to render only on props passed to other components

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,24 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
 // Component Dfinition
-function Nav() {
-  // create categories
-  const categories = [
-    {
-      name: "commercial",
-      description: "Photos of grocery stores, food trucks, and other commercial projects",
-    },
-    { name: "portraits", description: "Portraits of people in my life" },
-    { name: "food", description: "Delicious delicacies" },
-    {
-      name: "landscape",
-      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-    },
-  ];
+function Nav(props) {
+  const { categories = [], setCurrentCategory, currentCategory } = props; // destructure the props
 
-  function categorySelected(name) {
-    console.log(`${name} clicked`);
-  }
+  //useEffic - a hook to trigger  a re-render on a variable value change. Difference to useState is
+  //          its an API that reflectst he lifecycle methods of the component
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]); // callback function followed by second argument that directs the hook
+  // to re-render the component on changes to the value of this state,
+  // meaning the if CURRENTCATEGORY changes, the component will re-render
 
   //react components must always return a single parent JSX element
 
@@ -42,13 +36,19 @@ function Nav() {
             <span>Contact</span>
           </li>
           {categories.map((category) => (
-            <li className="mx-1" key={category.name}>
+            <li
+              /** if the currentCategory name is equal the category name reutnr 'navAcitve' */
+              className={`mx-1 ${currentCategory.name === category.name && "navActive"}`}
+              key={category.name}
+            >
               {/* WHY???? 
                  The onClick() attribute is expecting a callback function declaration. It's 
                   important that we wrap it in a function declaration rather than just calling
                   categorySelected(category.name), which would cause the function to get called 
                   whenever the component renders as well.*/}
-              <span onClick={() => categorySelected(category.name)}>{category.name}</span>
+              <span onClick={() => setCurrentCategory(category.name)}>
+                {capitalizeFirstLetter(category.name)}
+              </span>
             </li>
           ))}
         </ul>
